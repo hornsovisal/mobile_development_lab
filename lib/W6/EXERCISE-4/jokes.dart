bc3c736905150c165740fd681f50740b92718b12
hornsovisal <hornsovisal@github.com>
2026-06-13 13:35:09 +0700

    add W6

--- lib/W6/EXERCISE-4/jokes.dart
+++ lib/W6/EXERCISE-4/jokes.dart
@@ -1,78 +1,29 @@
 class Joke {
   final String title;
   final String description;
-
   const Joke({required this.title, required this.description});
 }
 
+//20 Joke
 const List<Joke> jokes = [
-  Joke(
-    title: "Group Project",
-    description: "Group Project មាន ៥ នាក់ តែ Git commit មានតែ ១ នាក់។",
-  ),
-  Joke(
-    title: "Group Project",
-    description: "Group Project មាន ៥ នាក់ តែ Git commit មានតែ ១ នាក់។",
-  ),
-
-  Joke(
-    title: "Group Project",
-    description: "Group Project មាន ៥ នាក់ តែ Git commit មានតែ ១ នាក់។",
-  ),
-  Joke(
-    title: "Group Project",
-    description: "Group Project មាន ៥ នាក់ តែ Git commit មានតែ ១ នាក់។",
-  ),
-  Joke(
-    title: "Group Project",
-    description: "Group Project មាន ៥ នាក់ តែ Git commit មានតែ ១ នាក់។",
-  ),
-  Joke(
-    title: "Group Project",
-    description: "Group Project មាន ៥ នាក់ តែ Git commit មានតែ ១ នាក់។",
-  ),
-  Joke(
-    title: "Group Project",
-    description: "Group Project មាន ៥ នាក់ តែ Git commit មានតែ ១ នាក់។",
-  ),
-  Joke(
-    title: "Group Project",
-    description: "Group Project មាន ៥ នាក់ តែ Git commit មានតែ ១ នាក់។",
-  ),
-  Joke(
-    title: "Group Project",
-    description: "Group Project មាន ៥ នាក់ តែ Git commit មានតែ ១ នាក់។",
-  ),
-  Joke(
-    title: "Group Project",
-    description: "Group Project មាន ៥ នាក់ តែ Git commit មានតែ ១ នាក់។",
-  ),
-  Joke(
-    title: "Group Project",
-    description: "Group Project មាន ៥ នាក់ តែ Git commit មានតែ ១ នាក់។",
-  ),
-  Joke(
-    title: "Group Project",
-    description: "Group Project មាន ៥ នាក់ តែ Git commit មានតែ ១ នាក់។",
-  ),
-  Joke(
-    title: "Group Project",
-    description: "Group Project មាន ៥ នាក់ តែ Git commit មានតែ ១ នាក់។",
-  ),
-  Joke(
-    title: "Group Project",
-    description: "Group Project មាន ៥ នាក់ តែ Git commit មានតែ ១ នាក់។",
-  ),
-  Joke(
-    title: "Group Project",
-    description: "Group Project មាន ៥ នាក់ តែ Git commit មានតែ ១ នាក់។",
-  ),
-  Joke(
-    title: "Group Project",
-    description: "Group Project មាន ៥ នាក់ តែ Git commit មានតែ ១ នាក់។",
-  ),
-  Joke(
-    title: "Group Project",
-    description: "Group Project មាន ៥ នាក់ តែ Git commit មានតែ ១ នាក់។",
-  ),
+  Joke(title: "Joke 1 ", description: "This is Joke។"),
+  Joke(title: "Joke 2 ", description: "This is Joke។"),
+  Joke(title: "Joke 4 ", description: "This is Joke។"),
+  Joke(title: "Joke 5 ", description: "This is Joke។"),
+  Joke(title: "Joke 6 ", description: "This is Joke។"),
+  Joke(title: "Joke 7 ", description: "This is Joke។"),
+  Joke(title: "Joke 1 ", description: "This is Joke។"),
+  Joke(title: "Joke 8 ", description: "This is Joke។"),
+  Joke(title: "Joke 9 ", description: "This is Joke។"),
+  Joke(title: "Joke 10 ", description: "This is Joke។"),
+  Joke(title: "Joke 11", description: "This is Joke។"),
+  Joke(title: "Joke 12 ", description: "This is Joke។"),
+  Joke(title: "Joke 13 ", description: "This is Joke។"),
+  Joke(title: "Joke 14 ", description: "This is Joke។"),
+  Joke(title: "Joke 15 ", description: "This is Joke។"),
+  Joke(title: "Joke 16 ", description: "This is Joke។"),
+  Joke(title: "Joke 17 ", description: "This is Joke។"),
+  Joke(title: "Joke 18 ", description: "This is Joke។"),
+  Joke(title: "Joke 19 ", description: "This is Joke។"),
+  Joke(title: "Joke 20 ", description: "This is Joke។"),
 ];
